7994f13f7537d9c146d4eec03ed8f920a4a701d2
imkgk <lnz013@gmail.com>
2021-01-27 10:10:32 +0800

    use markdown to render fund description (#2)
    Co-authored-by: anonymouse <anonymouse@x.com>

--- src/share_data.js
+++ src/share_data.js
@@ -2,11 +2,8 @@ export const FUNDS_DATA = {
   jiuyao: {
     t2: {
       name: "91Coin BTC 趋势基金",
-      code: "TFB200102",
-      style: "进取型",
-      leverage: "2.00",
-      exposure: "90%",
-      liquidation: "0.7",
+      manager: "jiuyao",
+      code: "t2"
     },
   }
 };
